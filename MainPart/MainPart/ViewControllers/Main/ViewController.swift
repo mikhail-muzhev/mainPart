19ccf4b0db97d5acac487831f7e468b3e03ec67b
Mikhail Muzhev <mikhail.muzhev@ltst.ru>
2019-09-30 20:35:13 +0600

    Add Localizable strings

--- MainPart/MainPart/ViewControllers/Main/ViewController.swift
+++ MainPart/MainPart/ViewControllers/Main/ViewController.swift
@@ -12,6 +12,8 @@ import AVKit
 
 class ViewController: BaseViewController {
 
+    // MARK: - Views
+
     private var videoView: VideoView = {
         let videoView = VideoView(filePath: Bundle.main.path(forResource: "intro", ofType: "mp4")!)
         videoView.alpha = 0
@@ -20,20 +22,31 @@ class ViewController: BaseViewController {
 
     private lazy var backgroundView: UIView = {
         let view = UIView()
-        view.backgroundColor = UIColor.white.withAlphaComponent(0.3)
         view.alpha = 0
         view.isHidden = true
         return view
     }()
 
     private lazy var titleLabel: AttributedLabel = {
-        return AttributedLabel(text: "Text", style: .blackMediumLeft17)
+        let label = AttributedLabel(text: R.string.localizable.main_intro_title(),
+                                    style: LabelStyle.blackBoldLeft32)
+        return label
+    }()
+
+    private lazy var descriptionLabel: AttributedLabel = {
+        let label = AttributedLabel(text: R.string.localizable.main_intro_description(),
+                                    style: .blackMediumLeft20)
+        return label
     }()
 
     private lazy var okButton: AttributedButton = {
-        let button = AttributedButton(style: .lightGreenMediumCenter17)
-        button.setTitleForAllStates("Button")
+        let button = AttributedButton(style: .whiteMediumCenter20)
+        button.setTitleForAllStates(R.string.localizable.main_intro_ok_button_title())
         button.addTarget(self, action: #selector(okButtonDidPressed), for: .touchUpInside)
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.lawyerInPocketMainGray.cgColor
+        button.backgroundColor = .lawyerInPocketEmerald
+        button.layer.cornerRadius = 10
         return button
     }()
 
@@ -42,34 +55,22 @@ class ViewController: BaseViewController {
         return imageView
     }()
 
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         addSubviews()
+        videoView.delegate = self
         makeConstraints()
         animateVideoViewAppearing()
     }
 
-    private func animateVideoViewAppearing() {
-        UIView.animate(withDuration: 1, animations: { [weak videoView] in
-            videoView?.alpha = 1
-        }, completion: { [weak videoView] isCompleted in
-            videoView?.playVideo()
-        })
-    }
-
-    private func animateBackgroundViewAppearing() {
-        backgroundView.isHidden = false
-        view.bringSubviewToFront(backgroundView)
-        UIView.animate(withDuration: 1, animations: { [weak backgroundView] in
-            backgroundView?.alpha = 1
-        })
-    }
+    // MARK: - UI settings
 
     private func addSubviews() {
-        videoView.delegate = self
         view.addSubviews(videoView, backgroundView)
-        backgroundView.addSubviews(titleLabel, okButton)
+        backgroundView.addSubviews(titleLabel, descriptionLabel, okButton)
     }
 
     private func makeConstraints() {
@@ -80,14 +81,42 @@ class ViewController: BaseViewController {
             $0.edges.equalToSuperview()
         }
         titleLabel.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-12)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+        }
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(10)
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-12)
         }
         okButton.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-20)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-60)
+            $0.height.equalTo(54)
+            $0.leading.equalToSuperview().offset(20)
         }
     }
+
+    // MARK: - Animations
+
+    private func animateVideoViewAppearing() {
+           UIView.animate(withDuration: 1, animations: { [weak videoView] in
+               videoView?.alpha = 1
+           }, completion: { [weak videoView] isCompleted in
+               videoView?.playVideo()
+           })
+       }
+
+       private func animateBackgroundViewAppearing() {
+           backgroundView.isHidden = false
+           view.bringSubviewToFront(backgroundView)
+           UIView.animate(withDuration: 1, animations: { [weak backgroundView] in
+               backgroundView?.alpha = 1
+           })
+       }
+
+    // MARK: - Actions
 
     @objc private func okButtonDidPressed() {
         
